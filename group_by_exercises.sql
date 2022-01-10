df32abc76c554240df7f840528e022ba9e6d1405
Jesse Marder <jesse.d.marder@gmail.com>
2022-01-10 09:36:20 -0600

    Finish group by exercises

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -74,7 +74,52 @@ SELECT LOWER(CONCAT(SUBSTR(first_name,1,1),SUBSTR(last_name,1,4),"_",SUBSTR(birt
 FROM employees
 GROUP BY username;
 
-# 8 - Any duplicates? STILL WORKING ON THIS ONE!!!
+# 8 - Any duplicates? 
 SELECT LOWER(CONCAT(SUBSTR(first_name,1,1),SUBSTR(last_name,1,4),"_",SUBSTR(birth_date,6,2),SUBSTR(birth_date,3,2))) AS username, COUNT(*)
 FROM employees
-GROUP BY username;
+GROUP BY username
+HAVING COUNT(*)>1;
+# Yes, there are duplicates. 13251 row are returned by this query so there are those many duplicate usernames
+
+# 9 a - Avg Salary
+SELECT emp_no, AVG(salary)
+FROM salaries
+GROUP BY emp_no;
+
+# 9 b - Current employees in each department
+SELECT dept_no, COUNT(*)
+FROM dept_emp
+GROUP BY dept_no;
+
+# 9 c - How many salaries per employee
+SELECT emp_no, COUNT(salary)
+FROM salaries
+GROUP BY emp_no;
+
+# 9 d - Max salary for each employee
+SELECT emp_no, MAX(salary)
+FROM salaries
+GROUP BY emp_no;
+
+# 9 e - Min salary for each employee
+SELECT emp_no, MIN(salary)
+FROM salaries
+GROUP BY emp_no;
+
+# 9 f - Standard deviation of salary for each employee
+SELECT emp_no, STD(salary)
+FROM salaries
+GROUP BY emp_no;
+
+# 9 g - Max salary for each employee where max is > $150k
+SELECT emp_no, MAX(salary)
+FROM salaries
+GROUP BY emp_no
+HAVING MAX(salary)>150000;
+
+# 9 h - Average salary for each employee where avg is between $80-90k
+SELECT emp_no, AVG(salary)
+FROM salaries
+GROUP BY emp_no
+HAVING AVG(salary) BETWEEN 80000 AND 90000;
+
