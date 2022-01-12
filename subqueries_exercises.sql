45b471eb7085f9a9dbeb097cf4474fa7503fee89
Jesse Marder <jesse.d.marder@gmail.com>
2022-01-12 09:53:22 -0600

    Update subqueries exercises

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -25,18 +25,22 @@ WHERE emp_no IN (
 AND to_date> CURDATE();
 
 # 3 - How many people in employees table no longer working for the company?
-SELECT COUNT(DISTINCT emp_no)
+SELECT COUNT(emp_no)
 FROM employees 
-WHERE emp_no IN 
-	(SELECT emp_no 
-	FROM salaries 
-	WHERE to_date<CURDATE()) # This would result in everyone who has a salary end date in the past (which also includes those whose salaries changed)
-AND emp_no NOT IN (
+WHERE emp_no NOT IN (
 	SELECT emp_no 
 	FROM salaries 
-	WHERE to_date>CURDATE()); # Removes those who are still at the company
+	WHERE to_date>CURDATE()); # Removes those who are still at the company. If they don't have a salary with a to_date > CURDATE then they aren't at the company anymore. 
+	
+# Could also use dept_emp table
+SELECT COUNT(emp_no)
+FROM employees 
+WHERE emp_no NOT IN (
+	SELECT emp_no 
+	FROM dept_emp
+	WHERE to_date>CURDATE());	
+	
 # 59900 people no longer working for the company from employees
-
 
 # 4 - Find all current department managers that are female
 SELECT first_name, last_name
@@ -61,4 +65,49 @@ SELECT AVG(salary)
 FROM salaries; # Historical Average is 63810.7448
 
 # 6 - How many current salaries are within 1 std of current highest salary? What % of all salaries is this?
+SELECT COUNT(*) AS Within_1_STD,
+100*COUNT(*)/(SELECT COUNT(*)
+			FROM salaries
+				WHERE to_date>CURDATE()) AS Pct_of_all_salaries
+FROM salaries
+WHERE to_date>CURDATE() AND
+salary >= (
+(SELECT MAX(salary)
+	FROM salaries
+		WHERE to_date>CURDATE())-
+			(SELECT STD(salary)
+				FROM salaries
+					WHERE to_date>CURDATE()));
+# 83 salaries within 1 STD of highest, which is	0.0346% of all salaries
 
+# BONUS 1 - Find department names that currently have female managers
+
+SELECT dept_name AS Female_Managed_Departments
+FROM departments
+WHERE dept_no IN (
+SELECT dept_no
+FROM dept_manager
+WHERE to_date>CURDATE() AND
+emp_no IN 
+(SELECT emp_no
+FROM employees
+WHERE gender = 'F'));
+
+# BONUS 2 - Find first and last name of employee with highest salary
+SELECT first_name, last_name FROM employees
+WHERE emp_no = (
+SELECT emp_no
+FROM salaries
+WHERE salary = (SELECT MAX(salary) FROM salaries WHERE to_date>CURDATE()));
+
+# BONUS 3 - Department name of the employee with highest salary
+SELECT dept_name
+FROM departments
+WHERE dept_no = 
+(SELECT dept_no FROM dept_emp 
+WHERE to_date> CURDATE() AND
+emp_no = (
+SELECT emp_no
+FROM salaries
+WHERE 
+salary = (SELECT MAX(salary) FROM salaries WHERE to_date>CURDATE())));
